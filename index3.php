4468c98801073f2daff883baf2362f7dbef1abcc
Yusuf <yusuf@kidsoft.com.au>
2016-07-26 13:39:56 +1000

    added autoload

--- index3.php
+++ index3.php
@@ -2,7 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once('./vendor/smalot/pdfparser/src/Smalot/PdfParser/Parser.php');
+include './vendor/autoload.php';
+
 
 if (!isset($_GET['pdf'])) die('?pdf cannot empty');
 if (!file_exists ( './pdf/'.$_GET['pdf']  )) die('No pdf found');
@@ -10,8 +11,8 @@ if (!file_exists ( './pdf/'.$_GET['pdf']  )) die('No pdf found');
 $parser = new \Smalot\PdfParser\Parser();
 $pdf    = $parser->parseFile('./pdf/'.$_GET['pdf']);
 
-//$text = $pdf->getText();
-//var_dump($text);
+$text = $pdf->getText();
+var_dump($text);
 
 die("end");
 
