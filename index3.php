5a50cc29457383b97aee5d8cbc3d33f31debc541
Yusuf <yusuf@kidsoft.com.au>
2016-07-26 13:26:14 +1000

    Added pdfparser thru composer, dont forget to composer update

--- index3.php
+++ index3.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once('./PdfParser/Parser.php');
+require_once('./vendor/smalot/pdfparser/src/Smalot/PdfParser/Parser.php');
 
 if (!isset($_GET['pdf'])) die('?pdf cannot empty');
 if (!file_exists ( './pdf/'.$_GET['pdf']  )) die('No pdf found');
@@ -10,6 +10,8 @@ if (!file_exists ( './pdf/'.$_GET['pdf']  )) die('No pdf found');
 $parser = new \Smalot\PdfParser\Parser();
 $pdf    = $parser->parseFile('./pdf/'.$_GET['pdf']);
 
-$text = $pdf->getText();
-echo $text;
+//$text = $pdf->getText();
+//var_dump($text);
+
+die("end");
 
